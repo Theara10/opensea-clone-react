96f990aa43b9393150e348536227bef1582e4d47
Theara10 <thearatheng10@gmail.com>
2021-11-23 20:06:31 +0700

    nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,36 @@
-import React from 'react'
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import React from "react";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import SearchIcon from "@mui/icons-material/Search";
 
-function Navbar(){
-    return(
-        <div className="nav">
-            
-            <div className="nav-left">
-                <img/>
-                <h3>OpenSea</h3>
-            </div>
-            <div className="nav-right">
-                <input placeholder="Search" />
-                <div className="nav-menu">
-                    <p>Explore</p>
-                    <p>Stats</p>
-                    <p>Resources</p>
-                    <p>Create</p>
-                    <AccountCircleIcon className="nav-menu-icon"/>
-                    <AccountBalanceWalletIcon className="nav-menu-icon"/>
-                </div>
-            </div>
+function Navbar() {
+  return (
+    <div className="nav">
+      <div className="nav-left">
+        <img
+          src="https://opensea.io/static/images/logos/opensea.svg"
+          height="40"
+          width="auto"
+        />
+        <h3>OpenSea</h3>
+      </div>
+      <div className="nav-right">
+        <div className="search-box">
+          <SearchIcon />
+          <input placeholder="search-input" />
         </div>
-    )
+
+        <div className="nav-menu">
+          <p>Explore</p>
+          <p>Stats</p>
+          <p>Resources</p>
+          <p>Create</p>
+          <AccountCircleIcon className="nav-menu-icon" />
+          <AccountBalanceWalletIcon className="nav-menu-icon" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
