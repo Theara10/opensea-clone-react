2ef9941c6059341025fc3315e0843c5d0118e91c
Theara10 <thearatheng10@gmail.com>
2021-11-30 07:47:17 +0700

    add home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import SlowMotionVideoIcon from '@mui/icons-material/SlowMotionVideo';
 
 function Home() {
     return (
@@ -6,23 +7,27 @@ function Home() {
             <section className="home-banner">
                 <div className="home-banner-left">
                     <h2>Discover, collect, and sell extraordinary NFTs</h2>
-                    <p>on the world's first & largest NFT marketplace</p>
-                    <div>
-                        <button>Explore</button>
-                        <button>Create</button>
+                    <p>on the world's first & <br/>largest NFT marketplace</p>
+                    <div className="home-banner-bottons">
+                        <button className="explore-button">Explore</button>
+                        <button className="create-button">Create</button>
+                    </div>
+                    <div className="home-banner-left-learnmore">
+                        <SlowMotionVideoIcon className="video-icon"/>
+                        <p>Learn more about Opensea</p>
                     </div>
                 </div>
                 <div className="home-banner-right">
                     <div className="home-banner-right-card">
-                        <img src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F6170e01f8d7639b95a7f2eeb%2FSotheby-s-NFT-Natively-Digital-1-2-sale-Bored-Ape-Yacht-Club--8817-by-Yuga-Labs%2F0x0.png%3Ffit%3Dscale"
-                         height="" width="auto"
+                        <img src="https://lh6.ggpht.com/HlgucZ0ylJAfZgusynnUwxNIgIp5htNhShF559x3dRXiuy_UdP3UQVLYW6c=s1200"
+                         height="400" width="100%" style={{borderTopRightRadius: 10, borderTopLeftRadius: 10}}
                         />
-                        <div>
+                        <div className="home-banner-right-bottom">
                             <img src="https://lh3.googleusercontent.com/sASTPVLSZm4udp-lrQOyJDRp3ut7xxsQbnafH0XPEtGtjupaL7TjJFl2dav5FLRbNdwpUWCrA31AO8OgUrAMWofB7iVHlMeWbHfm=s80" 
                                 height="30" width="auto" />
-                            <div>
-                                <p>GMA_048_002</p>
-                                <p>Jesuperman</p>
+                            <div className="home-banner-right-bottom-profile">
+                                <p className="home-banner-profile-name">GMA_048_002</p>
+                                <p className="home-banner-profile-desc">Jesuperman</p>
                             </div>
                         </div>
                     </div>
